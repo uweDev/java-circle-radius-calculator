aaa83be315125c6cd6f885e54b3400dd5fabc58b
Uwe Hellmann <58147568+uweDev@users.noreply.github.com>
2020-12-09 11:57:03 +0100

    improve readability

--- kreisradiusApp.java
+++ kreisradiusApp.java
@@ -14,7 +14,7 @@ public class kreisradiusApp {
 		double circleRadiusDouble, scopeDouble, surfaceDouble;
 		
 		// Input windows
-		unitString			= JOptionPane.showInputDialog("Einheit eingeben: ");
+		unitString		= JOptionPane.showInputDialog("Einheit eingeben: ");
 		circleRadiusString	= JOptionPane.showInputDialog("Kreisradius eingeben: ");
 
 		// Replaces comma with period and change the data type
@@ -22,19 +22,19 @@ public class kreisradiusApp {
 		circleRadiusDouble	= Double.valueOf(circleRadiusString);
 		
 		// Calculate the result
-		scopeDouble			= 2.0 * 3.1415926 * circleRadiusDouble;
-		surfaceDouble		= 3.1415926 * circleRadiusDouble * circleRadiusDouble;
+		scopeDouble	= 2.0 * 3.1415926 * circleRadiusDouble;
+		surfaceDouble	= 3.1415926 * circleRadiusDouble * circleRadiusDouble;
 		
 		// Change the data types and replaces period with comma
-		scopeString			= String.valueOf(scopeDouble);
-		surfaceString		= String.valueOf(surfaceDouble);
-		scopeString			= scopeString.replace('.', ',');
-		surfaceString		= surfaceString.replace('.', ',');
+		scopeString	= String.valueOf(scopeDouble);
+		surfaceString	= String.valueOf(surfaceDouble);
+		scopeString	= scopeString.replace('.', ',');
+		surfaceString	= surfaceString.replace('.', ',');
 		
 		// Results output
 		JOptionPane.showMessageDialog(
 			null, "Ihr Ergebnis lautet: \n"
 			+ "Umfang: " + scopeString + " " + unitString + "\n"
-			+ "Fl‰che: " + surfaceString + " " + unitString + '\u00b2');
+			+ "Fl√§che: " + surfaceString + " " + unitString + '\u00b2');
 	}
 }
